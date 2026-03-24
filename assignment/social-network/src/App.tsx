5a46470131c2046faaf9e2082b27ebf73dc994ef
Nik Pushkarski <11289396+nikpushkarski@users.noreply.github.com>
2026-03-23 20:44:21 -0400

    add the Home page

--- assignment/social-network/src/App.tsx
+++ assignment/social-network/src/App.tsx
@@ -1,8 +1,30 @@
+import Home from "./Home"
+import doomGuy from "./assets/doom_face.png"
+
+
 function App() {
 
   return (
     <>
-      <h1>Social Network</h1>
+      <img 
+        src={doomGuy} alt="the Doom Guy face with yellow eyes" 
+        width="25 rem"
+        style={{display: "inline-block"}}
+      />
+      <h1 
+        style={{
+          display: "inline-block",
+          color: "#7a0000"
+        }}
+      >
+        FRIENDS IN DOOM
+      </h1>
+      <img 
+        src={doomGuy} alt="the Doom Guy face with yellow eyes" 
+        width="25 rem"
+        style={{display: "inline-block"}}
+      />
+      <Home></Home>
     </>
   )
 }
---
+++ assignment/social-network/src/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home () {
+  return (
+    <>
+      <h2>The government blocked your fav social media?</h2>
+      <h2>Join "Friends in doom" today!</h2>
+      <p>We can't be blocked. Cause we have nothing to lose</p>
+    </>
+  )
+}
