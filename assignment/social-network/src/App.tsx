0a88fcfb665f09c0ed782e1d59a4fa0e03e56ea3
Nik Pushkarski <11289396+nikpushkarski@users.noreply.github.com>
2026-03-23 21:08:57 -0400

    add About and fixes the routing

--- assignment/social-network/src/App.tsx
+++ assignment/social-network/src/App.tsx
@@ -1,11 +1,12 @@
-import Home from "./Home"
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom"
 import doomGuy from "./assets/doom_face.png"
-
+import Home from "./Home"
+import About from "./About"
 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <img 
         src={doomGuy} alt="the Doom Guy face with yellow eyes" 
         width="25 rem"
@@ -24,8 +25,14 @@ function App() {
         width="25 rem"
         style={{display: "inline-block"}}
       />
-      <Home></Home>
-    </>
+      <div>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
