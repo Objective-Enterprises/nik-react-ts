0001cade321e77825409f21f18925b56a2fa1c54
Nik Pushkarski <11289396+nikpushkarski@users.noreply.github.com>
2026-03-24 12:43:47 -0400

    Implement the addUser function

--- assignment/social-network/src/AddUserForm.tsx
+++ assignment/social-network/src/AddUserForm.tsx
@@ -2,14 +2,18 @@ import { useState } from "react";
 
 export default function AddUserForm () {
   const [userName, setUserName] = useState("");
+  const [users, setUsers] = useState([""]);
   function addUser (userName: string) {
-    
+    const newUsers = [...users, userName];
+    setUsers(newUsers);
+    setUserName("");
   }
   return (
     <>
       <form onSubmit={
         e => {
           e.preventDefault();
+          addUser(userName);
         }
       }>
         <input 
