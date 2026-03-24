bb03c781370cf10f4a414dca1833783973bc4fa0
Nik Pushkarski <11289396+nikpushkarski@users.noreply.github.com>
2026-03-23 21:45:21 -0400

    Create the Contact page

--- assignment/social-network/src/App.tsx
+++ assignment/social-network/src/App.tsx
@@ -2,35 +2,32 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom"
 import doomGuy from "./assets/doom_face.png"
 import Home from "./Home"
 import About from "./About"
+import Contact from "./Contact"
+import "./App.css"
 
 function App() {
-
   return (
     <BrowserRouter>
       <img 
         src={doomGuy} alt="the Doom Guy face with yellow eyes" 
         width="25 rem"
-        style={{display: "inline-block"}}
       />
-      <h1 
-        style={{
-          display: "inline-block",
-          color: "#7a0000"
-        }}
-      >
+      <h1 className="titleHeader">
         FRIENDS IN DOOM
       </h1>
       <img 
         src={doomGuy} alt="the Doom Guy face with yellow eyes" 
         width="25 rem"
-        style={{display: "inline-block"}}
       />
       <div>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+        <Link to="/">Home</Link> | 
+        <Link to="/about">About</Link> |
+        <Link to="/contact">Contact</Link>
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   )
